99a9ed38fe5593e3cfe888cb2edaa9d6ea5cd09a
jarferh <jarferhharoun1@gmail.com>
2024-08-03 18:43:09 +0100

    change in slip printing

--- slip-printing.php
+++ slip-printing.php
@@ -73,11 +73,13 @@
             background-image: url(./assets/images/login/login.jpg);
             overflow: hidden;
         }
-        .inp{
-           height: 100%;
+
+        .inp {
+            height: 100%;
         }
-        .bt{
-           height: 100%;
+
+        .bt {
+            height: 100%;
         }
     </style>
 </head>
@@ -239,6 +241,25 @@
                         <!-- Google Map will be inserted here -->
                     </div>
                 </div>
+            </div>
+        </div>
+        <div class="wv">
+            <div class="z-n1">
+                <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+                    <defs>
+                        <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+                    </defs>
+                    <g class="parallax">
+                        <use xlink:href="#gentle-wave" x="48" y="0" fill="rgba(69,148,77,0.7" />
+                        <use xlink:href="#gentle-wave" x="48" y="3" fill="rgba(69,148,77,0.5)" />
+                        <use xlink:href="#gentle-wave" x="48" y="5" fill="rgba(69,148,77,0.3)" />
+                        <use xlink:href="#gentle-wave" x="48" y="7" fill="#45944d" />
+                    </g>
+                </svg>
+            </div>
+            <!--Waves end-->
+            <div class="content flex">
+                <!-- <p>By.Goodkatz | Free to use </p> -->
             </div>
         </div>
     </footer>
