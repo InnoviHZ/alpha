44304c775bdd46efb47fe2e2b4c23b31e76ccd58
musty131311 <mustyazare@gmail.com>
2024-08-03 15:07:01 +0000

    test1

--- slip-printing.php
+++ slip-printing.php
@@ -27,7 +27,9 @@
                 background-position: 0% 50%;
             }
         }
-
+        .new{
+            
+        }
         .body {
             /* background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab); */
             background-color: var(--white);
