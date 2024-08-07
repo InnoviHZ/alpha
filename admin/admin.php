3cbcb77f40b5ef845ab1a4a18b6ecbd3c948a99f
musty131311 <mustyazare@gmail.com>
2024-08-07 15:58:33 +0000

    user infor

--- admin/admin.php
+++ admin/admin.php
@@ -68,8 +68,8 @@
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
       <!-- Brand Logo -->
       <a href="index.html" class="brand-link">
-        <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">AdminLTE 3</span>
+        <img src="../assets/images/logo/logo1.svg" alt="Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <span class="brand-text font-weight-light">DASHBOARD</span>
       </a>
 
       <!-- Sidebar -->
@@ -81,6 +81,7 @@
           </div>
           <div class="info">
             <a href="#" class="d-block">Alexander Pierce</a>
+            <span class="badge badge-info">Administrator</span>
           </div>
         </div>
 
